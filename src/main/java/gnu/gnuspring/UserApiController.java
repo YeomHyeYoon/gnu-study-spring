a800ae407ad8c761ce5b13e180aa5af7575ee827
Yuni <hyeyoon5019@naver.com>
2021-07-17 15:56:05 +0900

    modify USerApiController.java

--- src/main/java/gnu/gnuspring/UserApiController.java
+++ src/main/java/gnu/gnuspring/UserApiController.java
@@ -1,11 +1,8 @@
 package gnu.gnuspring;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class UserApiController {
@@ -17,8 +14,6 @@ public class UserApiController {
         user.setPassword("pass");
         user.setEmail("111");
         user.setId(1);
-       // Map<String,Object> map = new HashMap<>();
-       // map.put("user",user);
         return user;
     }
 
